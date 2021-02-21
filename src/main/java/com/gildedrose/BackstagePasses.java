0e700c3b761d08ba88f4fd7922553437538f22d6
carrymaniac <838537276@qq.com>
2021-02-21 17:02:49 +0800

    refactor BackstagePasses

--- src/main/java/com/gildedrose/BackstagePasses.java
+++ src/main/java/com/gildedrose/BackstagePasses.java
@@ -1,7 +1,33 @@
 package com.gildedrose;
 
 public class BackstagePasses extends Item {
+
     public BackstagePasses(int sellIn, int quality) {
         super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+    }
+
+    @Override
+    void updateQualityForItem() {
+        if (quality < 50) {
+            quality = quality + 1;
+            if (sellIn < 11 && quality < 50) {
+                quality = quality + 1;
+            }
+            if (sellIn < 6 && quality < 50) {
+                quality = quality + 1;
+            }
+        }
+    }
+
+    @Override
+    void updateSellIn() {
+        sellIn = sellIn - 1;
+    }
+
+    @Override
+    void updateQualityIfSellInLessThanZero() {
+        if (sellIn < 0) {
+            quality = 0;
+        }
     }
 }
