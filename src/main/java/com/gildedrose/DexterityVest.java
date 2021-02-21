d4fda736e094978e1401709990beb74c2823d82e
carrymaniac <838537276@qq.com>
2021-02-21 17:08:09 +0800

    refactor DexterityVest

--- src/main/java/com/gildedrose/DexterityVest.java
+++ src/main/java/com/gildedrose/DexterityVest.java
@@ -4,4 +4,23 @@ public class DexterityVest extends Item {
     public DexterityVest(int sellIn, int quality) {
         super("+5 Dexterity Vest", sellIn, quality);
     }
+
+    @Override
+    void updateQualityForItem() {
+        if (quality > 0) {
+            quality = quality - 1;
+        }
+    }
+
+    @Override
+    void updateSellIn() {
+        sellIn = sellIn - 1;
+    }
+
+    @Override
+    void updateQualityIfSellInLessThanZero() {
+        if (sellIn < 0 && quality > 0) {
+            quality = quality - 1;
+        }
+    }
 }
--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -16,12 +16,12 @@ public class TexttestFixture {
         out.println("OMGHAI!");
 
         Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
+                new DexterityVest(10, 20), //
                 new AgedBrie(2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
                 new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new BackstagePasses(15, 20),
                 new BackstagePasses(10, 49),
                 new BackstagePasses(5, 49),
                 // this conjured item does not work properly yet
