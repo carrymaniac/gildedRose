85c07f563f1a8dabf020d7efd783e4f95b3afce8
carrymaniac <838537276@qq.com>
2021-02-21 16:58:05 +0800

    refactor AgedBrie

--- src/main/java/com/gildedrose/AgedBrie.java
+++ src/main/java/com/gildedrose/AgedBrie.java
@@ -6,5 +6,23 @@ public class AgedBrie extends Item {
         super("Aged Brie", sellIn, quality);
     }
 
+    @Override
+    void updateQualityForItem() {
+        if (quality < 50) {
+            quality = quality + 1;
+        }
+    }
 
+    @Override
+    void updateSellIn() {
+        sellIn = sellIn - 1;
+    }
+
+    @Override
+    void updateQualityIfSellInLessThanZero() {
+        if (sellIn < 0 && quality < 50) {
+            quality = quality + 1;
+        }
+    }
 }
+
