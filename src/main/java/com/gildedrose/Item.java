4fa34a71d4efe5a88ac6b779344389777d652e8b
carrymaniac <838537276@qq.com>
2021-02-24 21:23:26 +0800

    remove useless code

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -1,6 +1,6 @@
 package com.gildedrose;
 
-public class Item {
+public abstract class Item {
 
     public String name;
 
@@ -19,73 +19,9 @@ public class Item {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 
-    boolean isEquals(String s) {
-        return s.equals(name);
-    }
+    abstract void updateQualityIfSellInLessThanZero();
 
-    boolean isAged_brie() {
-        return isEquals("Aged Brie");
-    }
+    abstract void updateSellIn();
 
-    boolean isBackstagePasses() {
-        return isEquals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    boolean isSulfuras() {
-        return isEquals("Sulfuras, Hand of Ragnaros");
-    }
-
-    void updateQualityIfSellInLessThanZero() {
-        if (sellIn < 0) {
-            if (!isAged_brie()) {
-                if (!isBackstagePasses()) {
-                    if (quality > 0) {
-                        if (!isSulfuras()) {
-                            quality = quality - 1;
-                        }
-                    }
-                } else {
-                    quality = 0;
-                }
-            } else {
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
-            }
-        }
-    }
-
-    void updateSellIn() {
-        if (!isSulfuras()) {
-            sellIn = sellIn - 1;
-        }
-    }
-
-    void updateQualityForItem() {
-        if (!isAged_brie() && !isBackstagePasses()) {
-            if (quality > 0) {
-                if (!isSulfuras()) {
-                    quality = quality - 1;
-                }
-            }
-        } else {
-            if (quality < 50) {
-                quality = quality + 1;
-
-                if (isBackstagePasses()) {
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-    }
+    abstract void updateQualityForItem();
 }
